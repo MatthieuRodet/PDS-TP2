b2a0696ce8b2090f4eaa988ac6485da5a0a94746
MatthieuRodet <matthieurodetts@gmail.com>
2021-10-07 11:40:32 +0200

    prettier printer

--- prettyprinter.ml
+++ prettyprinter.ml
@@ -3,10 +3,10 @@ open ASD
 (* main function. return only a string *)
 let rec prettyprint ast =
   match ast with
-  | AddExpression (l, r) -> "(" ^ (prettyprint l) ^ " + " ^ (prettyprint r) ^ ")"
-  | MinusExpression (l, r) -> "(" ^ (prettyprint l) ^ " - " ^ (prettyprint r) ^ ")"
-  | MulExpression (l, r) -> "(" ^ (prettyprint l) ^ " * " ^ (prettyprint r) ^ ")"
-  | DivExpression (l, r) -> "(" ^ (prettyprint l) ^ " / " ^ (prettyprint r) ^ ")"
+  | AddExpression (l, r) -> (prettyprint l) ^ " + " ^ (prettyprint r)
+  | MinusExpression (l, r) -> (prettyprint l) ^ " - " ^ (prettyprint r)
+  | MulExpression (l, r) -> (prettyprint l) ^ " * " ^ (prettyprint r)
+  | DivExpression (l, r) -> (prettyprint l) ^ " / " ^ (prettyprint r)
   | ParentheseExpression (exp) -> "(" ^ (prettyprint exp) ^ ")"
   | IntegerExpression i -> string_of_int i
 
