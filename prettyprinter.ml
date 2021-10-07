304e9dc348e283e0288e121828bcb229ccb3eee0
MatthieuRodet <matthieurodetts@gmail.com>
2021-10-07 11:02:06 +0200

    pretty print of first operations

--- prettyprinter.ml
+++ prettyprinter.ml
@@ -4,6 +4,10 @@ open ASD
 let rec prettyprint ast =
   match ast with
   | AddExpression (l, r) -> "(" ^ (prettyprint l) ^ " + " ^ (prettyprint r) ^ ")"
+  | MinusExpression (l, r) -> "(" ^ (prettyprint l) ^ " - " ^ (prettyprint r) ^ ")"
+  | MulExpression (l, r) -> "(" ^ (prettyprint l) ^ " * " ^ (prettyprint r) ^ ")"
+  | DivExpression (l, r) -> "(" ^ (prettyprint l) ^ " / " ^ (prettyprint r) ^ ")"
+  | ParentheseExpression (exp) -> "(" ^ (prettyprint exp) ^ ")"
   | IntegerExpression i -> string_of_int i
 
 (* TODO : extend when you extend the language *)
