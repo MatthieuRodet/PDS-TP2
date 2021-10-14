dc0ac3d72ee4e1efb7c0baf84c97ca1065b8a387
MatthieuRodet <matthieurodetts@gmail.com>
2021-10-14 14:35:36 +0200

    PrettyPrettyPrinter :)

--- prettyprinter.ml
+++ prettyprinter.ml
@@ -3,26 +3,26 @@ open ASD
 (* main function. return only a string *)
 let rec prettyprint ast = match ast with 
   |Prog([]) -> ""
-  |Prog(a::q) -> "{\n" ^ prettyprint_block a ^ "}\n" ^  prettyprint (Prog(q)) 
+  |Prog(a::q) -> "{\n" ^ prettyprint_block a 0 ^ "}\n" ^  prettyprint (Prog(q)) 
 
-and prettyprint_block ast = match ast with 
+and prettyprint_block ast n = match ast with 
   |Unit([],[]) -> ""
-  |Unit([],[a]) -> prettyprint_instr a ^ "\n"
-  |Unit([], a::q) -> prettyprint_instr a ^ "\n" ^ prettyprint_block (Unit([], q)) 
-  |Unit([a],l) -> prettyprint_declar a ^ "\n" ^ prettyprint_block (Unit([], l))
-  |Unit(a::q, l) -> prettyprint_declar a ^ "\n" ^ prettyprint_block (Unit(q,l))
+  |Unit([],[a]) -> prettyprint_instr a (n+1)
+  |Unit([], a::q) -> prettyprint_instr a (n+1) ^ prettyprint_block (Unit([], q)) n
+  |Unit([a],l) -> prettyprint_declar a (n+1) ^ "\n" ^ prettyprint_block (Unit([], l)) n
+  |Unit(a::q, l) -> prettyprint_declar a (n+1) ^ prettyprint_block (Unit(q,l)) n
 
-and prettyprint_instr ast = match ast with 
-  | Affect(var, expr) -> prettyprint_variable var ^ " := " ^ prettyprint_expression expr
-  | Print(l) -> "PRINT " ^ prettyprint_print l 
-  | Read(l) -> "READ " ^ prettyprint_read l 
-  | If(expr, i1, i2) -> prettyprint_if expr i1 i2 
-   |While(exp, instr) -> "While " ^ prettyprint_expression exp ^ "{" ^ prettyprint_instr instr ^ "}"
-  |Block(b) -> "{" ^ prettyprint_block b ^ "}"
+and prettyprint_instr ast n = match ast with 
+  | Affect(var, expr) -> tabs n ^ prettyprint_variable var ^ " := " ^ prettyprint_expression expr ^ "\n"
+  | Print(l) -> tabs n ^ "PRINT " ^ prettyprint_print l ^ "\n"
+  | Read(l) -> tabs n ^ "READ " ^ prettyprint_read l ^ "\n"
+  | If(expr, i1, i2) -> prettyprint_if expr i1 i2 n
+  | While(exp, instr) -> tabs n ^ "While " ^ prettyprint_expression exp ^ " DO\n" ^ prettyprint_instr instr (n+1) ^ tabs n ^ "DONE\n"
+  | Block(b) -> tabs n ^ "{\n" ^ prettyprint_block b n ^ tabs n ^ "}\n"
 
-and prettyprint_if expr i1 i2 = match i2 with 
-        |Some(x) -> "If " ^ prettyprint_expression expr ^ "THEN " ^ prettyprint_instr i1 ^ "ELSE " ^ prettyprint_instr x ^ " FI" 
-        |None -> "If " ^ prettyprint_expression expr ^ "THEN " ^ prettyprint_instr i1 ^ " Fi" 
+and prettyprint_if expr i1 i2 n = match i2 with 
+        |Some(x) -> tabs n ^ "IF " ^ prettyprint_expression expr ^ " THEN\n" ^ prettyprint_instr i1 (n+1) ^ tabs n ^ "ELSE\n" ^ prettyprint_instr x (n+1) ^ tabs n ^ "FI\n" 
+        |None -> tabs n ^ "IF " ^ prettyprint_expression expr ^ " THEN\n" ^ prettyprint_instr i1 (n+1) ^ tabs n ^ "FI\n" 
   
 and prettyprint_print l = match l with 
   |[] -> ""
@@ -37,9 +37,9 @@ and prettyprint_read a = match a with
   |[a] -> prettyprint_variable a 
   |a::q -> prettyprint_variable a ^ ", " ^ prettyprint_read q 
 
-and prettyprint_declar ast = match ast with 
+and prettyprint_declar ast n = match ast with 
   |Declaration([]) -> ""
-  |Declaration(l) -> "INT " ^ prettyprint_many_variables l
+  |Declaration(l) -> tabs n ^ "INT " ^ prettyprint_many_variables l ^ "\n"
 
 and prettyprint_many_variables  ast = match ast with 
   |[] -> ""
@@ -60,4 +60,8 @@ and prettyprint_expression ast =
   | IntegerExpression i -> string_of_int i
   | VarExpression i -> i
 
+and tabs n =
+  match n with
+  | 0 -> ""
+  | _ -> "\t" ^ tabs (n-1)
 (* TODO : extend when you extend the language *)
